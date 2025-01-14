47d4cdc6472cafede108529d5a2d2a091fc92f8f
Amin Mahmoodian <a.mahmoodiyan@gmail.com>
2025-01-14 15:27:47 +0100

    adding license

--- startup/middlewares.js
+++ startup/middlewares.js
@@ -1,10 +1,10 @@
-const helmet = require('helmet');
+const helmet = require("helmet");
 
-module.exports = function(app, express){
-    app.set('view engine', 'pug');
-    app.set('views', './views');
-
-    app.use(express.json());
-    app.use(helmet());
-    app.use(express.urlencoded({ extended: true }));
-}
+module.exports = function (app, express) {
+  app.set("view engine", "pug");
+  app.set("views", "./views");
+  app.use(express.json());
+  app.use(helmet());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.static("public"));
+};
--- startup/routes.js
+++ startup/routes.js
@@ -1,7 +1,8 @@
-const home = require('../routes/home');
-const error = require('../middlewares/error');
+const home = require("../routes/home");
+const error = require("../middlewares/error");
 
-module.exports = function(app){
-    app.use('/', home);
-    app.use(error);
-}
+module.exports = function (app) {
+  debugger;
+  app.use("/", home);
+  app.use(error);
+};
